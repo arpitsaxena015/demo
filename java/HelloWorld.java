7dbcd2625fdd82ab24c0f78fddd85c47bc3f5bca
arpitsaxena015 <arpitsaxenamarch1996@gmail.com>
2025-07-31 12:50:22 +0530

    java hello world

---
+++ java/HelloWorld.java
@@ -0,0 +1,7 @@
+package demo.java;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
--- java/hello_world.java
+++
@@ -1,9 +0,0 @@
-package demo.java;
-
-public class hello_world  {
-
-    public Static void main (String[]args){
-        System.out.println("Hello world");
-    }
-    
-}
